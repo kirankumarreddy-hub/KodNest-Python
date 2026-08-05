791734f3dee7e5e9afa80a71df3241fcfdf7867b
R Kiran Kumar Reddy <kiran99120948@gmail.com>
2026-08-05 16:04:15 +0530

    push

--- M01/T01/SRC/Welcome to Kodnest.py
+++ M01/T01/SRC/Welcome to Kodnest.py
@@ -1,5 +1,29 @@
 '''print("kodnest")'''
 
-first_name = "kiran"
+'''first_name = "kiran"
 last_name = "kumar"
 print(first_name,last_name) 
+'''
+
+'''a = 10
+b = 20
+c = a+b
+print(c)
+
+
+marks = 35
+result = marks >= 40
+print(result)'''
+
+name = 'Kiran'
+print(name, type(name))
+
+is_student = True
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score,type(score))
